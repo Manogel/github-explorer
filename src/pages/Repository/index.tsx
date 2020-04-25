18f54397246c09a586766173ad744dc6155ade52
Manogel <manoelgomes53@gmail.com>
2020-04-25 00:36:28 -0300

    repository styling page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
-// import { Container } from './styles';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issues } from './styles';
+import logoimg from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -9,7 +11,54 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoimg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://cdn.discordapp.com/avatars/473900543769837593/866025cf2440f1fe4b1764d835f85687?size=512"
+            alt="Profile"
+          />
+          <div>
+            <strong>Rocketseat/unform</strong>
+            <p>deescricao</p>
+          </div>
+        </header>
+
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to="/">
+          <div>
+            <strong>sdsd</strong>
+            <p>sdsds</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
